f33d46862e00b6c91920ec42af42fa70c314d2ce
TerryLin2001 <83539616+TerryLin2001@users.noreply.github.com>
2023-01-17 11:00:22 +0800

    Update GetTopo.cshtml.cs

--- SDNweb/Pages/GetTopo.cshtml.cs
+++ SDNweb/Pages/GetTopo.cshtml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Globalization;
 using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace SDNweb.Pages
 {
@@ -21,9 +24,16 @@ namespace SDNweb.Pages
 
         public void OnPostGetData()
         {
-            ViewData["Devices"] = GetONOSInformation("devices");
-            ViewData["Hosts"] = GetONOSInformation("hosts");
-            ViewData["Links"] = GetONOSInformation("links");
+            String hosts = GetONOSInformation("hosts");
+            String Devices = GetONOSInformation("devices");
+            String Links = GetONOSInformation("links");
+            String Deviceids = DevicestoDevicesid(Devices);
+
+            ViewData["Devices"] = Devices;
+            ViewData["Hosts"] = hosts;
+            ViewData["Links"] = Links;
+            ViewData["Deviceids"] = Deviceids;
+
 
             string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
             ViewData["TimeStamp"] = dateTime;
@@ -40,11 +50,33 @@ namespace SDNweb.Pages
 
             using (var client = new HttpClient(httpClientHandler))
             {
-                var endpoint = new Uri("http://192.168.98.129:8181/onos/v1/"+getItem);
+                var endpoint = new Uri("http://192.168.48.129:8181/onos/v1/"+getItem);
                 var result = client.GetAsync(endpoint).Result;
-                var json = result.Content.ReadAsStringAsync().Result;
-                return json;
+                var jsonstring = result.Content.ReadAsStringAsync().Result;
+                return jsonstring;
             }
+        }
+
+        public String DevicestoDevicesid(String devices)
+        {
+            List<string> deviceids = new List<string>();
+
+            foreach (var item in devices.Replace("}{", "}|{").Split('|'))
+            {
+               String line = item;
+                //Console.WriteLine(line);
+
+                dynamic dline = JObject.Parse(line);
+                String device = Convert.ToString(dline.devices[0]);
+                dynamic ddevice = JObject.Parse(device);
+                //Console.WriteLine("test:" + ddevice.id);
+                String deviceid = Convert.ToString(ddevice.id);
+                deviceids.Add(deviceid);
+            }
+
+
+            var result = String.Join(", ", deviceids.ToArray());
+            return result;
         }
 
 
